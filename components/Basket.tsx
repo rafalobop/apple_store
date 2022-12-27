921edd8a680ee3a066ddc603ffac0f3dc8f11b55
Rafa <rloboplaza@proveedor.teco.com.ar>
2022-12-27 18:04:45 -0300

    basket in progress

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import { FiShoppingBag } from 'react-icons/fi'
+import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectBasketItems, selectBasketTotal } from '../redux/basketSlice'
 
 function Basket() {
+
+  const items = useSelector(selectBasketItems)
+  const basketTotal = useSelector(selectBasketTotal)
+
+  if(!items.length) return null
+
   return (
-    <div>Basket</div>
+    <Link href="/checkout">
+        <div className='fixed bottom-10 right-10 z-50 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-gray-300' >
+          {items.length > 0 && (
+            <span className='absolute -right-2 -top-2 z-50 flex h-7 w-7 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-white'>{items.length}</span>
+          )}
+        </div>
+    </Link>
   )
 }
 
